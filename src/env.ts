a78a671242f210985bce715335f12a98db1e7c42
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-04-01 19:43:08 -0300

    fix: workflow

--- src/env.ts
+++ src/env.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
 	PORT: z.coerce.number().default(3333),
-	DATABASE_URL: z.string().url(),
-	DATABASE_URL_DOCKER: z.string().url(),
+	DATABASE_URL: z.string(),
+	DATABASE_URL_DOCKER: z.string(),
 	REDIS_HOST: z.string(),
 	REDIS_PORT: z.coerce.number().default(6379),
 	REDIS_CACHED_KEY: z.string(),
