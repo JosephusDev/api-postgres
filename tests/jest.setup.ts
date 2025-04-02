3a63275ed8fc5762fc42b3e0eecdb99c2aa756b0
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-04-02 03:26:39 -0300

    fix: jest.setup.ts

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,11 +1,16 @@
 import supertest from 'supertest'
 import { app } from '../src/app'
 import client from '../src/config/redis'
+import { env } from '../src/env'
+
+// Aumenta o timeout para hooks para 10 segundos
+jest.setTimeout(30000)
 
 beforeAll(async () => {
 	try {
 		await client.connect().catch(err => {
 			console.warn('Redis connection failed, proceeding with tests:', err.message)
+			console.log(env.REDIS_HOST, env.DATABASE_URL, env.REDIS_PASSWORD, env.REDIS_PORT, env.REDIS_USERNAME)
 		})
 		await app.ready()
 	} catch (error) {
